67ad23f526ffd88010bf03ec10b7d0043a7ad697
Bianca <biancabatinas64@gmail.com>
2023-04-26 23:33:27 +0300

    update menu, error photon.pun

---
+++ My project/Assets/Character/Scripts/Launcher.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallBack
+{
+    public static Launcher instance;
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject loadingScreen;
+    public TMP_Text loadingtext;
+
+    public GameObject menuButtons;
+    // Start is called before the first frame update 
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
