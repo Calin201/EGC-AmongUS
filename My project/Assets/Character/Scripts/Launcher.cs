fd5bfc9ab6b994ba579ff918c4838e4364585dab
Bianca <biancabatinas64@gmail.com>
2023-04-27 00:17:50 +0300

    update: loading screen, network connection, create room,

--- My project/Assets/Character/Scripts/Launcher.cs
+++ My project/Assets/Character/Scripts/Launcher.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
-public class Launcher : MonoBehaviourPunCallBack
+public class Launcher : MonoBehaviourPunCallbacks
 {
     public static Launcher instance;
     public void Awake()
@@ -16,12 +17,67 @@ public class Launcher : MonoBehaviourPunCallBack
     public TMP_Text loadingtext;
 
     public GameObject menuButtons;
+
+    public GameObject createRoomScreen;
+    public TMP_InputField roomInput;
+
+
     // Start is called before the first frame update 
     void Start()
     {
+        CloseMenu();
+
+        loadingScreen.SetActive(true);
+        loadingtext.text = "Connecting To Network...";
+
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+    void CloseMenu()
+    {
+        loadingScreen.SetActive(false);
+        menuButtons.SetActive(false);
+        createRoomScreen.SetActive(false);
 
     }
 
+    public override void OnConnectedToMaster()
+    {
+       
+
+        PhotonNetwork.JoinLobby();
+        loadingtext.text = "Joining lobby...";
+
+    }
+
+    public override void OnJoinedLobby()
+    {
+        CloseMenu();
+        menuButtons.SetActive(true);
+    }
+
+    public void OpenRoomCreate()
+    {
+        CloseMenu();
+        createRoomScreen.SetActive(true);
+
+    }
+
+    public void CreateRoom()
+    {
+        if(!string.IsNullOrEmpty(roomInput.text))
+        {
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = 2;
+            
+            PhotonNetwork.CreateRoom(roomInput.text,options);
+
+            CloseMenu();
+            loadingtext.text = "Creating Room...";
+            loadingScreen.SetActive(true);
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
